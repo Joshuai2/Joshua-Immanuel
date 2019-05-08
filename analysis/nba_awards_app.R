c2a43b663994e7025006c6bf52bdbe8cd924143c
Joshua Immanuel <joshuai2@illinois.edu>
2019-05-08 15:33:11 +0000

    added abit of event reactive

--- analysis/nba_awards_app.R
+++ analysis/nba_awards_app.R
@@ -8,22 +8,75 @@ ui = fluidPage(
       h3("Award Selection"),
       
       # Dropdowns
-      selectInput("aw", "What award do you want to predict?", 
+      selectInput(inputId ="award", "What award do you want to predict?", 
                   choices = c("MVP", "Rookie of the Year", "Defensive Player of the Year",
                               "Sixth Man", "Most Improved Player")),
       
       numericInput(inputId = "year", label = ("Year (e.g. 1980)"), 
                    value = 2019, min = 1989, max = 2019),
       
-      submitButton("Load Preview Awards")
+      actionButton("load", "Load Preview Awards")
     ),
     
-    mainPanel("CONTENT GOES HERE")
+    mainPanel(
+      h3("Top Five Predictions"),
+      verbatimTextOutput("top_predictions"),
+      
+      h3("Radar Plot"),
+      plotOutout("radar")
+    )
   )
 )
 
 # Define server logic ----
 server = function(input, output, session) {
+  
+  active_awards = eventReactive(input$load, {
+    input$award
+  })
+  
+  active_dataset = eventReactive(input$load, {
+    switch(paste0("players_", input$year),
+           "players_1989" = players_1989,
+           "players_1990" = players_1990,
+           "players_1991" = players_1991,
+           "players_1992" = players_1992,
+           "players_1993" = players_1993,
+           "players_1994" = players_1994,
+           "players_1995" = players_1995,
+           "players_1996" = players_1996,
+           "players_1997" = players_1997,
+           "players_1998" = players_1998,
+           "players_1999" = players_1999,
+           "players_2000" = players_2000,
+           "players_2001" = players_2001,
+           "players_2002" = players_2002,
+           "players_2003" = players_2003,
+           "players_2004" = players_2004,
+           "players_2005" = players_2005,
+           "players_2006" = players_2006,
+           "players_2007" = players_2007,
+           "players_2008" = players_2008,
+           "players_2009" = players_2009,
+           "players_2010" = players_2010,
+           "players_2011" = players_2011,
+           "players_2012" = players_2012,
+           "players_2013" = players_2013,
+           "players_2014" = players_2014,
+           "players_2015" = players_2015,
+           "players_2016" = players_2016,
+           "players_2017" = players_2017,
+           "players_2018" = players_2018,
+           "players_2019" = players_2019,
+           
+           )
+  })
+  
+  output$top_predictions = renderPrint(
+    if(active_dataset() = players_2019){
+      
+    }
+  )
   
 }
 
